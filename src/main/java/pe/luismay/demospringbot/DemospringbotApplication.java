09e067fe911278e25ac08ac1937048ea69fb2783
Link May <90990023+Link-May@users.noreply.github.com>
2023-04-26 20:52:55 -0600

    nombre o hola mundo

--- src/main/java/pe/luismay/demospringbot/DemospringbotApplication.java
+++ src/main/java/pe/luismay/demospringbot/DemospringbotApplication.java
@@ -2,17 +2,23 @@ package pe.luismay.demospringbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
+@Controller
 @SpringBootApplication
 public class DemospringbotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemospringbotApplication.class, args);
 	}
+
 	
-	@GetMapping
-	String holaMundo(){
-		return "hola mundo";
+	@GetMapping("/")
+	String holaMundo(@RequestParam(defaultValue = "mundo") String nombre, Model model) {
+		model.addAttribute("nombre", nombre + ", Bienvenido!");
+		return "hola-mundo";
 	}
 }
